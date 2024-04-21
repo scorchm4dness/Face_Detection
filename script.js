827b4d6306f1c818eb27a97b3440f4a6069e3a33
scorchm4dness <nyrallehoran@gmail.com>
2024-04-21 22:02:58 +0800

    face-detection-js

---
+++ script.js
@@ -0,0 +1,72 @@
+const video = document.getElementById("video");
+
+Promise.all([
+  faceapi.nets.ssdMobilenetv1.loadFromUri("./models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
+  faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('./models')
+]).then(startVideo);
+
+function startVideo() {
+  navigator.mediaDevices
+    .getUserMedia({
+      video: true,
+      audio: false,
+    })
+    .then((stream) => {
+      video.srcObject = stream;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+function getLabeledFaceDescriptions() {
+  const labels = ["Cyrille"];
+  return Promise.all(
+    labels.map(async (label) => {
+      const descriptions = [];
+      for (let i = 1; i <= labels.length; i++) {
+        const img = await faceapi.fetchImage(`./labels/${label}/${i}.png`);
+        const detections = await faceapi
+          .detectSingleFace(img)
+          .withFaceLandmarks()
+          .withFaceDescriptor();
+        descriptions.push(detections.descriptor);
+      }
+      return new faceapi.LabeledFaceDescriptors(label, descriptions);
+    })
+  );
+}
+
+video.addEventListener("play", async () => {
+  const labeledFaceDescriptors = await getLabeledFaceDescriptions();
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors);
+
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video)
+      .withFaceLandmarks()
+      .withFaceExpressions()
+      .withFaceDescriptors();
+
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+
+    resizedDetections.forEach(detection => {
+      const box = detection.detection.box;
+      const drawBox = new faceapi.draw.DrawBox(box, { label: faceMatcher.findBestMatch(detection.descriptor) });
+      drawBox.draw(canvas);  
+   });
+
+  }, 100);
+});
